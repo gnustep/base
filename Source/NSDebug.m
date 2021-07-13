gnustep/libs-base

Add support for native locks on Windows

Source/NSDebug.m
@@ -93,7 +93,7 @@ static table_entry*	the_table = 0;
 static BOOL	debug_allocation = NO;
 static BOOL	debug_byte_size = NO;
 
-static pthread_mutex_t	uniqueLock;
+static gs_recursive_mutex_t	uniqueLock;
 
 static void     _GSDebugAllocationFetch(list_entry *items, BOOL difference);
 static void     _GSDebugAllocationFetchAll(list_entry *items);
@@ -106,8 +106,8 @@ static void (*_GSDebugAllocationAddFunc)(Class c, id o)
 static void (*_GSDebugAllocationRemoveFunc)(Class c, id o)
   = _GSDebugAllocationRemove;
 
-#define doLock() pthread_mutex_lock(&uniqueLock)
-#define unLock() pthread_mutex_unlock(&uniqueLock)
+#define doLock() GS_RECURSIVE_MUTEX_LOCK(uniqueLock)
+#define unLock() GS_RECURSIVE_MUTEX_UNLOCK(uniqueLock)
 
 @interface GSDebugAlloc : NSObject
 + (void) initialize;
@@ -116,7 +116,7 @@ static void (*_GSDebugAllocationRemoveFunc)(Class c, id o)
 @implementation GSDebugAlloc
 + (void) initialize
 {
-  GS_INIT_RECURSIVE_MUTEX(uniqueLock);
+  GS_RECURSIVE_MUTEX_INIT(uniqueLock);
 }
 @end
 

Source/NSException.m
@@ -491,7 +491,7 @@ static void find_address (bfd *abfd, asection *section,
 
 @end
 
-static pthread_mutex_t	        modLock;
+static gs_recursive_mutex_t	  modLock;
 static NSMutableDictionary	*stackModules = nil;
 
 // initialize stack trace info
@@ -500,7 +500,7 @@ GSLoadModule(NSString *fileName)
 {
   GSBinaryFileInfo	*module = nil;
 
-  (void)pthread_mutex_lock(&modLock);
+  GS_RECURSIVE_MUTEX_LOCK(modLock);
 
   if (stackModules == nil)
     {
@@ -549,7 +549,7 @@ GSLoadModule(NSString *fileName)
 	    }
 	}
     }
-  (void)pthread_mutex_unlock(&modLock);
+  GS_RECURSIVE_MUTEX_UNLOCK(modLock);
 
   if (module == (id)[NSNull null])
     {
@@ -564,9 +564,9 @@ GSListModules()
   NSArray	*result;
 
   GSLoadModule(nil);	// initialise
-  (void)pthread_mutex_lock(&modLock);
+  GS_RECURSIVE_MUTEX_LOCK(modLock);
   result = [stackModules allValues];
-  (void)pthread_mutex_unlock(&modLock);
+  GS_RECURSIVE_MUTEX_UNLOCK(modLock);
   return result;
 }
 
@@ -615,7 +615,7 @@ static SymInitializeType initSym = 0;
 static SymSetOptionsType optSym = 0;
 static SymFromAddrType fromSym = 0;
 static HANDLE	hProcess = 0;
-static	pthread_mutex_t	traceLock;
+static	gs_recursive_mutex_t	traceLock;
 #define	MAXFRAMES 62	/* Limitation of windows-xp */
 #else
 #define MAXFRAMES 128   /* 1KB buffer on 64bit machine */
@@ -902,7 +902,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 #if	defined(_WIN32) && !defined(USE_BFD)
   NSUInteger	addr[MAXFRAMES];
 
-  (void)pthread_mutex_lock(&traceLock);
+  GS_RECURSIVE_MUTEX_LOCK(traceLock);
   if (0 == hProcess)
     {
       hProcess = GetCurrentProcess();
@@ -916,7 +916,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to load kernel32.dll with error: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  capture = (CaptureStackBackTraceType)GetProcAddress(
@@ -925,7 +925,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find RtlCaptureStackBackTrace: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  hModule = LoadLibrary("dbghelp.dll");
@@ -933,7 +933,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to load dbghelp.dll with error: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  optSym = (SymSetOptionsType)GetProcAddress(
@@ -942,7 +942,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find SymSetOptions: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  initSym = (SymInitializeType)GetProcAddress(
@@ -951,7 +951,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find SymInitialize: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  fromSym = (SymFromAddrType)GetProcAddress(
@@ -960,7 +960,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find SymFromAddr: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	}
@@ -972,13 +972,13 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	  fprintf(stderr, "SymInitialize failed with error: %d\n",
 	    (int)GetLastError());
 	  fromSym = 0;
-	  (void)pthread_mutex_unlock(&traceLock);
+	  GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 	  return 0;
 	}
     }
   if (0 == capture)
     {
-      (void)pthread_mutex_unlock(&traceLock);
+      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
       return 0;
     }
 
@@ -989,7 +989,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
       memcpy(*returns, addr, numReturns * sizeof(void*));
     }
   
-  (void)pthread_mutex_unlock(&traceLock);
+  GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
 
 #elif	defined(HAVE_BACKTRACE)
   void          *addr[MAXFRAMES*sizeof(void*)];
@@ -1112,10 +1112,10 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 + (void) initialize
 {
 #if	defined(_WIN32) && !defined(USE_BFD)
-  GS_INIT_RECURSIVE_MUTEX(traceLock);
+  GS_RECURSIVE_MUTEX_INIT(traceLock);
 #endif
 #if     defined(USE_BFD)
-  GS_INIT_RECURSIVE_MUTEX(modLock);
+  GS_RECURSIVE_MUTEX_INIT(modLock);
 #endif
 }
 
@@ -1257,7 +1257,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
       symbol->MaxNameLen = 1024;
       symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
 
-      (void)pthread_mutex_lock(&traceLock);
+      GS_RECURSIVE_MUTEX_LOCK(traceLock);
       for (i = 0; i < count; i++)
         {
           NSUInteger	addr = (NSUInteger)*ptrs++; 
@@ -1273,7 +1273,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
                 @"unknown - %p", symbol->Name, addr];
             }
         }
-      (void)pthread_mutex_unlock(&traceLock);
+      GS_RECURSIVE_MUTEX_UNLOCK(traceLock);
       free(symbol);
 
       symbols = [[NSArray alloc] initWithObjects: syms count: count];

Source/NSTimeZone.m
@@ -289,7 +289,7 @@ static NSMutableDictionary *abbreviationDictionary = nil;
 static NSMutableDictionary *abbreviationMap = nil;
 
 /* Lock for creating time zones. */
-static pthread_mutex_t zone_mutex;
+static gs_recursive_mutex_t zone_mutex;
 
 static Class	NSTimeZoneClass;
 static Class	GSPlaceholderTimeZoneClass;
@@ -387,14 +387,14 @@ static NSString *_time_zone_path(NSString *subpath, NSString *type)
    * Return a cached time zone if possible.
    * NB. if data of cached zone does not match new data ... don't use cache
    */
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   zone = [zoneDictionary objectForKey: name];
   if (data != nil && [data isEqual: [zone data]] == NO)
     {
       zone = nil;
     }
   IF_NO_GC(RETAIN(zone));
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
 
   if (zone == nil)
     {
@@ -657,9 +657,9 @@ static NSMapTable	*absolutes = 0;
 {
   if (offset != uninitialisedOffset)
     {
-      pthread_mutex_lock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
       NSMapRemove(absolutes, (void*)(uintptr_t)offset);
-      pthread_mutex_unlock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
     }
   RELEASE(name);
   RELEASE(detail);
@@ -715,7 +715,7 @@ static NSMapTable	*absolutes = 0;
         }
     }
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   z = (GSAbsTimeZone*)NSMapGet(absolutes, (void*)(uintptr_t)anOffset);
   if (z != nil)
     {
@@ -766,7 +766,7 @@ static NSMapTable	*absolutes = 0;
           commonAbsolutes[index] = RETAIN(self);
         }
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
   return z;
 }
 
@@ -986,7 +986,7 @@ static NSMapTable	*absolutes = 0;
     {
       return abbreviationDictionary;
     }
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   if (abbreviationDictionary == nil)
     {
       NSAutoreleasePool	*pool = [NSAutoreleasePool new];
@@ -1043,7 +1043,7 @@ static NSMapTable	*absolutes = 0;
 	}
       [pool drain];
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
   return abbreviationDictionary;
 }
 
@@ -1061,7 +1061,7 @@ static NSMapTable	*absolutes = 0;
     {
       return abbreviationMap;
     }
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   if (abbreviationMap == nil)
     {
       NSAutoreleasePool		*pool = [NSAutoreleasePool new];
@@ -1095,7 +1095,7 @@ static NSMapTable	*absolutes = 0;
 #endif
 	  if (file == NULL)
 	    {
-              pthread_mutex_unlock(&zone_mutex);
+              GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
 	      [NSException
 		raise: NSInternalInconsistencyException
 		format: @"Failed to open time zone abbreviation map."];
@@ -1186,7 +1186,7 @@ static NSMapTable	*absolutes = 0;
         }
       [pool drain];
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
 
   return abbreviationMap;
 }
@@ -1204,7 +1204,7 @@ static NSMapTable	*absolutes = 0;
       return namesArray;
     }
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   if (namesArray == nil)
     {
       unsigned		i;
@@ -1230,7 +1230,7 @@ static NSMapTable	*absolutes = 0;
           RELEASE(ma);
         }
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
   return namesArray;
 }
 
@@ -1260,7 +1260,7 @@ static NSMapTable	*absolutes = 0;
 	   * locate the correct placeholder in the (lock protected)
 	   * table of placeholders.
 	   */
-          pthread_mutex_lock(&zone_mutex);
+          GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
 	  obj = (id)NSMapGet(placeholderMap, (void*)z);
 	  if (obj == nil)
 	    {
@@ -1271,7 +1271,7 @@ static NSMapTable	*absolutes = 0;
 	      obj = (id)NSAllocateObject(GSPlaceholderTimeZoneClass, 0, z);
 	      NSMapInsert(placeholderMap, (void*)z, (void*)obj);
 	    }
-          pthread_mutex_unlock(&zone_mutex);
+          GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
 	  return obj;
 	}
     }
@@ -1288,7 +1288,7 @@ static NSMapTable	*absolutes = 0;
 {
   NSTimeZone	*zone;
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   if (defaultTimeZone == nil)
     {
       zone = [self systemTimeZone];
@@ -1297,7 +1297,7 @@ static NSMapTable	*absolutes = 0;
     {
       zone = AUTORELEASE(RETAIN(defaultTimeZone));
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
   return zone;
 }
 
@@ -1306,7 +1306,7 @@ static NSMapTable	*absolutes = 0;
   if (self == [NSTimeZone class])
     {
       NSTimeZoneClass = self;
-      GS_INIT_RECURSIVE_MUTEX(zone_mutex);
+      GS_RECURSIVE_MUTEX_INIT(zone_mutex);
       GSPlaceholderTimeZoneClass = [GSPlaceholderTimeZone class];
       zoneDictionary = [[NSMutableDictionary alloc] init];
       [[NSObject leakAt: &zoneDictionary] release];
@@ -1351,9 +1351,9 @@ static NSMapTable	*absolutes = 0;
  */
 + (void) resetSystemTimeZone
 {
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   DESTROY(systemTimeZone);
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
   [[NSNotificationCenter defaultCenter]
     postNotificationName: NSSystemTimeZoneDidChangeNotification
                   object: nil];
@@ -1374,9 +1374,9 @@ static NSMapTable	*absolutes = 0;
 	{
 	  aTimeZone = [self systemTimeZone];
 	}
-      pthread_mutex_lock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
       ASSIGN(defaultTimeZone, aTimeZone);
-      pthread_mutex_unlock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
     }
 }
 
@@ -1387,7 +1387,7 @@ static NSMapTable	*absolutes = 0;
 {
   NSTimeZone	*zone = nil;
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   if (systemTimeZone == nil)
     {
       NSFileManager *dflt = [NSFileManager defaultManager];
@@ -1727,7 +1727,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
       ASSIGN(systemTimeZone, zone);
     }
   zone = AUTORELEASE(RETAIN(systemTimeZone));
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
   return zone;
 }
 
@@ -1746,7 +1746,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
     {
       return regionsArray;
     }
-  pthread_mutex_lock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
   if (regionsArray == nil)
     {
       NSAutoreleasePool	*pool = [NSAutoreleasePool new];
@@ -1778,7 +1778,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
 #endif
 	  if (fp == NULL)
 	    {
-              pthread_mutex_unlock(&zone_mutex);
+              GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
 	      [NSException
 		raise: NSInternalInconsistencyException
 		format: @"Failed to open time zone regions array file."];
@@ -1878,7 +1878,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
       regionsArray = [[NSArray alloc] initWithObjects: temp_array count: 24];
       [pool drain];
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
   return regionsArray;
 }
 
@@ -1921,9 +1921,9 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
     }
   else
     {
-      pthread_mutex_lock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
       zone = (NSTimeZone*)NSMapGet(absolutes, (void*)(uintptr_t)seconds);
-      pthread_mutex_unlock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
     }
   if (nil == zone)
     {
@@ -2403,7 +2403,7 @@ static NSString *zoneDirs[] = {
 
   if (beenHere == NO && tzdir == nil)
     {
-      pthread_mutex_lock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
       if (beenHere == NO && tzdir == nil)
 	{
 	  NSFileManager	*mgr = [NSFileManager defaultManager];
@@ -2424,7 +2424,7 @@ static NSString *zoneDirs[] = {
 	    }
 	  beenHere = YES;
 	}
-      pthread_mutex_unlock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
     }
   /* Use the system zone info if possible, otherwise, use our installed
      info.  */
@@ -3145,9 +3145,9 @@ newDetailInZoneForType(GSTimeZone *zone, TypeInfo *type)
 	  }
       }
 
-      pthread_mutex_lock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_LOCK(zone_mutex);
       [zoneDictionary setObject: self forKey: timeZoneName];
-      pthread_mutex_unlock(&zone_mutex);
+      GS_RECURSIVE_MUTEX_UNLOCK(zone_mutex);
     }
   NS_HANDLER
     {

gnustep/base

Source/NSDebug.m
@@ -93,7 +93,7 @@ static table_entry*	the_table = 0;
 static BOOL	debug_allocation = NO;
 static BOOL	debug_byte_size = NO;
 
-static pthread_mutex_t	uniqueLock;
+static gs_mutex_t	uniqueLock;
 
 static void     _GSDebugAllocationFetch(list_entry *items, BOOL difference);
 static void     _GSDebugAllocationFetchAll(list_entry *items);
@@ -106,8 +106,8 @@ static void (*_GSDebugAllocationAddFunc)(Class c, id o)
 static void (*_GSDebugAllocationRemoveFunc)(Class c, id o)
   = _GSDebugAllocationRemove;
 
-#define doLock() pthread_mutex_lock(&uniqueLock)
-#define unLock() pthread_mutex_unlock(&uniqueLock)
+#define doLock() GS_MUTEX_LOCK(uniqueLock)
+#define unLock() GS_MUTEX_UNLOCK(uniqueLock)
 
 @interface GSDebugAlloc : NSObject
 + (void) initialize;
@@ -116,7 +116,7 @@ static void (*_GSDebugAllocationRemoveFunc)(Class c, id o)
 @implementation GSDebugAlloc
 + (void) initialize
 {
-  GS_INIT_RECURSIVE_MUTEX(uniqueLock);
+  GS_MUTEX_INIT_RECURSIVE(uniqueLock);
 }
 @end
 

Source/NSException.m
@@ -491,7 +491,7 @@ static void find_address (bfd *abfd, asection *section,
 
 @end
 
-static pthread_mutex_t	        modLock;
+static gs_mutex_t	  modLock;
 static NSMutableDictionary	*stackModules = nil;
 
 // initialize stack trace info
@@ -500,7 +500,7 @@ GSLoadModule(NSString *fileName)
 {
   GSBinaryFileInfo	*module = nil;
 
-  (void)pthread_mutex_lock(&modLock);
+  GS_MUTEX_LOCK(modLock);
 
   if (stackModules == nil)
     {
@@ -549,7 +549,7 @@ GSLoadModule(NSString *fileName)
 	    }
 	}
     }
-  (void)pthread_mutex_unlock(&modLock);
+  GS_MUTEX_UNLOCK(modLock);
 
   if (module == (id)[NSNull null])
     {
@@ -564,9 +564,9 @@ GSListModules()
   NSArray	*result;
 
   GSLoadModule(nil);	// initialise
-  (void)pthread_mutex_lock(&modLock);
+  GS_MUTEX_LOCK(modLock);
   result = [stackModules allValues];
-  (void)pthread_mutex_unlock(&modLock);
+  GS_MUTEX_UNLOCK(modLock);
   return result;
 }
 
@@ -615,7 +615,7 @@ static SymInitializeType initSym = 0;
 static SymSetOptionsType optSym = 0;
 static SymFromAddrType fromSym = 0;
 static HANDLE	hProcess = 0;
-static	pthread_mutex_t	traceLock;
+static	gs_mutex_t	traceLock;
 #define	MAXFRAMES 62	/* Limitation of windows-xp */
 #else
 #define MAXFRAMES 128   /* 1KB buffer on 64bit machine */
@@ -902,7 +902,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 #if	defined(_WIN32) && !defined(USE_BFD)
   NSUInteger	addr[MAXFRAMES];
 
-  (void)pthread_mutex_lock(&traceLock);
+  GS_MUTEX_LOCK(traceLock);
   if (0 == hProcess)
     {
       hProcess = GetCurrentProcess();
@@ -916,7 +916,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to load kernel32.dll with error: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  capture = (CaptureStackBackTraceType)GetProcAddress(
@@ -925,7 +925,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find RtlCaptureStackBackTrace: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  hModule = LoadLibrary("dbghelp.dll");
@@ -933,7 +933,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to load dbghelp.dll with error: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  optSym = (SymSetOptionsType)GetProcAddress(
@@ -942,7 +942,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find SymSetOptions: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  initSym = (SymInitializeType)GetProcAddress(
@@ -951,7 +951,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find SymInitialize: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	  fromSym = (SymFromAddrType)GetProcAddress(
@@ -960,7 +960,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	    {
 	      fprintf(stderr, "Failed to find SymFromAddr: %d\n",
 		(int)GetLastError());
-	      (void)pthread_mutex_unlock(&traceLock);
+	      GS_MUTEX_UNLOCK(traceLock);
 	      return 0;
 	    }
 	}
@@ -972,13 +972,13 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 	  fprintf(stderr, "SymInitialize failed with error: %d\n",
 	    (int)GetLastError());
 	  fromSym = 0;
-	  (void)pthread_mutex_unlock(&traceLock);
+	  GS_MUTEX_UNLOCK(traceLock);
 	  return 0;
 	}
     }
   if (0 == capture)
     {
-      (void)pthread_mutex_unlock(&traceLock);
+      GS_MUTEX_UNLOCK(traceLock);
       return 0;
     }
 
@@ -989,7 +989,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
       memcpy(*returns, addr, numReturns * sizeof(void*));
     }
   
-  (void)pthread_mutex_unlock(&traceLock);
+  GS_MUTEX_UNLOCK(traceLock);
 
 #elif	defined(HAVE_BACKTRACE)
   void          *addr[MAXFRAMES*sizeof(void*)];
@@ -1112,10 +1112,10 @@ GSPrivateReturnAddresses(NSUInteger **returns)
 + (void) initialize
 {
 #if	defined(_WIN32) && !defined(USE_BFD)
-  GS_INIT_RECURSIVE_MUTEX(traceLock);
+  GS_MUTEX_INIT_RECURSIVE(traceLock);
 #endif
 #if     defined(USE_BFD)
-  GS_INIT_RECURSIVE_MUTEX(modLock);
+  GS_MUTEX_INIT_RECURSIVE(modLock);
 #endif
 }
 
@@ -1257,7 +1257,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
       symbol->MaxNameLen = 1024;
       symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
 
-      (void)pthread_mutex_lock(&traceLock);
+      GS_MUTEX_LOCK(traceLock);
       for (i = 0; i < count; i++)
         {
           NSUInteger	addr = (NSUInteger)*ptrs++; 
@@ -1273,7 +1273,7 @@ GSPrivateReturnAddresses(NSUInteger **returns)
                 @"unknown - %p", symbol->Name, addr];
             }
         }
-      (void)pthread_mutex_unlock(&traceLock);
+      GS_MUTEX_UNLOCK(traceLock);
       free(symbol);
 
       symbols = [[NSArray alloc] initWithObjects: syms count: count];

Source/NSTimeZone.m
@@ -289,7 +289,7 @@ static NSMutableDictionary *abbreviationDictionary = nil;
 static NSMutableDictionary *abbreviationMap = nil;
 
 /* Lock for creating time zones. */
-static pthread_mutex_t zone_mutex;
+static gs_mutex_t zone_mutex;
 
 static Class	NSTimeZoneClass;
 static Class	GSPlaceholderTimeZoneClass;
@@ -387,14 +387,14 @@ static NSString *_time_zone_path(NSString *subpath, NSString *type)
    * Return a cached time zone if possible.
    * NB. if data of cached zone does not match new data ... don't use cache
    */
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   zone = [zoneDictionary objectForKey: name];
   if (data != nil && [data isEqual: [zone data]] == NO)
     {
       zone = nil;
     }
   IF_NO_GC(RETAIN(zone));
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
 
   if (zone == nil)
     {
@@ -657,9 +657,9 @@ static NSMapTable	*absolutes = 0;
 {
   if (offset != uninitialisedOffset)
     {
-      pthread_mutex_lock(&zone_mutex);
+      GS_MUTEX_LOCK(zone_mutex);
       NSMapRemove(absolutes, (void*)(uintptr_t)offset);
-      pthread_mutex_unlock(&zone_mutex);
+      GS_MUTEX_UNLOCK(zone_mutex);
     }
   RELEASE(name);
   RELEASE(detail);
@@ -715,7 +715,7 @@ static NSMapTable	*absolutes = 0;
         }
     }
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   z = (GSAbsTimeZone*)NSMapGet(absolutes, (void*)(uintptr_t)anOffset);
   if (z != nil)
     {
@@ -766,7 +766,7 @@ static NSMapTable	*absolutes = 0;
           commonAbsolutes[index] = RETAIN(self);
         }
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
   return z;
 }
 
@@ -986,7 +986,7 @@ static NSMapTable	*absolutes = 0;
     {
       return abbreviationDictionary;
     }
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   if (abbreviationDictionary == nil)
     {
       NSAutoreleasePool	*pool = [NSAutoreleasePool new];
@@ -1043,7 +1043,7 @@ static NSMapTable	*absolutes = 0;
 	}
       [pool drain];
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
   return abbreviationDictionary;
 }
 
@@ -1061,7 +1061,7 @@ static NSMapTable	*absolutes = 0;
     {
       return abbreviationMap;
     }
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   if (abbreviationMap == nil)
     {
       NSAutoreleasePool		*pool = [NSAutoreleasePool new];
@@ -1095,7 +1095,7 @@ static NSMapTable	*absolutes = 0;
 #endif
 	  if (file == NULL)
 	    {
-              pthread_mutex_unlock(&zone_mutex);
+              GS_MUTEX_UNLOCK(zone_mutex);
 	      [NSException
 		raise: NSInternalInconsistencyException
 		format: @"Failed to open time zone abbreviation map."];
@@ -1186,7 +1186,7 @@ static NSMapTable	*absolutes = 0;
         }
       [pool drain];
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
 
   return abbreviationMap;
 }
@@ -1204,7 +1204,7 @@ static NSMapTable	*absolutes = 0;
       return namesArray;
     }
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   if (namesArray == nil)
     {
       unsigned		i;
@@ -1230,7 +1230,7 @@ static NSMapTable	*absolutes = 0;
           RELEASE(ma);
         }
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
   return namesArray;
 }
 
@@ -1260,7 +1260,7 @@ static NSMapTable	*absolutes = 0;
 	   * locate the correct placeholder in the (lock protected)
 	   * table of placeholders.
 	   */
-          pthread_mutex_lock(&zone_mutex);
+          GS_MUTEX_LOCK(zone_mutex);
 	  obj = (id)NSMapGet(placeholderMap, (void*)z);
 	  if (obj == nil)
 	    {
@@ -1271,7 +1271,7 @@ static NSMapTable	*absolutes = 0;
 	      obj = (id)NSAllocateObject(GSPlaceholderTimeZoneClass, 0, z);
 	      NSMapInsert(placeholderMap, (void*)z, (void*)obj);
 	    }
-          pthread_mutex_unlock(&zone_mutex);
+          GS_MUTEX_UNLOCK(zone_mutex);
 	  return obj;
 	}
     }
@@ -1288,7 +1288,7 @@ static NSMapTable	*absolutes = 0;
 {
   NSTimeZone	*zone;
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   if (defaultTimeZone == nil)
     {
       zone = [self systemTimeZone];
@@ -1297,7 +1297,7 @@ static NSMapTable	*absolutes = 0;
     {
       zone = AUTORELEASE(RETAIN(defaultTimeZone));
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
   return zone;
 }
 
@@ -1306,7 +1306,7 @@ static NSMapTable	*absolutes = 0;
   if (self == [NSTimeZone class])
     {
       NSTimeZoneClass = self;
-      GS_INIT_RECURSIVE_MUTEX(zone_mutex);
+      GS_MUTEX_INIT_RECURSIVE(zone_mutex);
       GSPlaceholderTimeZoneClass = [GSPlaceholderTimeZone class];
       zoneDictionary = [[NSMutableDictionary alloc] init];
       [[NSObject leakAt: &zoneDictionary] release];
@@ -1351,9 +1351,9 @@ static NSMapTable	*absolutes = 0;
  */
 + (void) resetSystemTimeZone
 {
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   DESTROY(systemTimeZone);
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
   [[NSNotificationCenter defaultCenter]
     postNotificationName: NSSystemTimeZoneDidChangeNotification
                   object: nil];
@@ -1374,9 +1374,9 @@ static NSMapTable	*absolutes = 0;
 	{
 	  aTimeZone = [self systemTimeZone];
 	}
-      pthread_mutex_lock(&zone_mutex);
+      GS_MUTEX_LOCK(zone_mutex);
       ASSIGN(defaultTimeZone, aTimeZone);
-      pthread_mutex_unlock(&zone_mutex);
+      GS_MUTEX_UNLOCK(zone_mutex);
     }
 }
 
@@ -1387,7 +1387,7 @@ static NSMapTable	*absolutes = 0;
 {
   NSTimeZone	*zone = nil;
 
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   if (systemTimeZone == nil)
     {
       NSFileManager *dflt = [NSFileManager defaultManager];
@@ -1727,7 +1727,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
       ASSIGN(systemTimeZone, zone);
     }
   zone = AUTORELEASE(RETAIN(systemTimeZone));
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
   return zone;
 }
 
@@ -1746,7 +1746,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
     {
       return regionsArray;
     }
-  pthread_mutex_lock(&zone_mutex);
+  GS_MUTEX_LOCK(zone_mutex);
   if (regionsArray == nil)
     {
       NSAutoreleasePool	*pool = [NSAutoreleasePool new];
@@ -1778,7 +1778,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
 #endif
 	  if (fp == NULL)
 	    {
-              pthread_mutex_unlock(&zone_mutex);
+              GS_MUTEX_UNLOCK(zone_mutex);
 	      [NSException
 		raise: NSInternalInconsistencyException
 		format: @"Failed to open time zone regions array file."];
@@ -1878,7 +1878,7 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
       regionsArray = [[NSArray alloc] initWithObjects: temp_array count: 24];
       [pool drain];
     }
-  pthread_mutex_unlock(&zone_mutex);
+  GS_MUTEX_UNLOCK(zone_mutex);
   return regionsArray;
 }
 
@@ -1921,9 +1921,9 @@ localZoneString, [zone name], sign, s/3600, (s/60)%60);
     }
   else
     {
-      pthread_mutex_lock(&zone_mutex);
+      GS_MUTEX_LOCK(zone_mutex);
       zone = (NSTimeZone*)NSMapGet(absolutes, (void*)(uintptr_t)seconds);
-      pthread_mutex_unlock(&zone_mutex);
+      GS_MUTEX_UNLOCK(zone_mutex);
     }
   if (nil == zone)
     {
@@ -2403,7 +2403,7 @@ static NSString *zoneDirs[] = {
 
   if (beenHere == NO && tzdir == nil)
     {
-      pthread_mutex_lock(&zone_mutex);
+      GS_MUTEX_LOCK(zone_mutex);
       if (beenHere == NO && tzdir == nil)
 	{
 	  NSFileManager	*mgr = [NSFileManager defaultManager];
@@ -2424,7 +2424,7 @@ static NSString *zoneDirs[] = {
 	    }
 	  beenHere = YES;
 	}
-      pthread_mutex_unlock(&zone_mutex);
+      GS_MUTEX_UNLOCK(zone_mutex);
     }
   /* Use the system zone info if possible, otherwise, use our installed
      info.  */
@@ -3145,9 +3145,9 @@ newDetailInZoneForType(GSTimeZone *zone, TypeInfo *type)
 	  }
       }
 
-      pthread_mutex_lock(&zone_mutex);
+      GS_MUTEX_LOCK(zone_mutex);
       [zoneDictionary setObject: self forKey: timeZoneName];
-      pthread_mutex_unlock(&zone_mutex);
+      GS_MUTEX_UNLOCK(zone_mutex);
     }
   NS_HANDLER
     {